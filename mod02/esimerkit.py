4224d9448180feefa738c59e40390d81f4df2f0d
petyrfishfinger <jossu.haa@gmail.com>
2026-08-27 10:45:14 +0300

    lämpötilamuunnos

--- mod02/esimerkit.py
+++ mod02/esimerkit.py
@@ -1,5 +1,10 @@
-print("mitä tapahtuu?")
-print("Huomenta joillekin\nOlen Johanna")
-user = input("Syötä käyttäjätunnus: ")
-user = "gunther"
-print("Huomenta," + user)
+# Kirjoita ohjelma, joka kysyy käyttäjältä
+# lämpötilan fahrenheittina ja muuttaa sen
+# celsiukseksi
+
+print("Tämä ohjelma muuntaa fahrenheitit celsius-asteiksi.\n")
+fahrenheit = input("Anna lämpötila fahrenheit-yksikössä: ")
+
+celsius = (int(fahrenheit) - 32) * 5/9
+
+print(celsius)
