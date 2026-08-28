1b693c669b510c857c6be8d1d13b2efcf25c7613
petyrfishfinger <jossu.haa@gmail.com>
2026-08-28 17:11:19 +0300

    padlock_lots

--- mod03/3_5.py
+++ mod03/3_5.py
@@ -1,17 +1,27 @@
-# Write a program that asks the user to 
-# enter a mass in medieval units:
-# talents (leiviskä), pounds (naula), and lots (luoti).
-# The program converts the input to full kilograms and grams
-# and outputs the result to the user:
 # One talent is 20 pounds.
 # One pound is 32 lots.
-# One lot is 13,3 grams.
-
+# One lot is 13.3 grams.
 import math
 
 talents = input("Enter talents: ")
 pounds = input("Enter pounds: ")
 lots = input("Enter lots: ")
 
-lot_in_grams = float(lots)*13,3
-pound_in_lots = float()
+talents = float(talents)
+pounds = float(pounds)
+lots = float(lots)
+# In grams
+talents_in_grams = talents*20*32*13.3
+pounds_in_grams = pounds*32*13.3
+lots_in_grams = lots*13.3
+# In kg
+talents_in_kg = talents_in_grams/1000
+pounds_in_kg = pounds_in_grams/1000
+lots_in_kg = lots_in_grams/1000
+
+print(f"Talents in grams: {talents_in_grams:.2f}")
+print(f"Talents in kg: {talents_in_kg:.2f}")
+print(f"Pounds in grams: {pounds_in_grams:.2f}")
+print(f"Pounds in kg: {pounds_in_kg:.2f}")
+print(f"Lots in grams: {lots_in_grams:.2f}")
+print(f"Lots in kg: {lots_in_kg:.2f}")
--- mod03/3_6.py
+++ mod03/3_6.py
@@ -2,3 +2,10 @@
 # combinations of numbers for a combination lock:
 # a 3-digit code where each number is between 0 and 9.
 # a 4-digit code where each number is between 1 and 6.
+import random
+
+lock1 = f"{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}"
+lock2 = f"{random.randint(1,6)}{random.randint(1,6)}{random.randint(1,6)}{random.randint(1,6)}"
+
+print(f"Lock 1 combination: {lock1}")
+print(f"Lock 2 combination: {lock2}")
