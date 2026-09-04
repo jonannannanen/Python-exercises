84864c3f6982f2ed35531ef3bf8bcd219e32e7ad
petyrfishfinger <jossu.haa@gmail.com>
2026-09-04 13:24:53 +0300

    projektinimijaikä

---
+++ peliprojekti/nimi_ikä.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 12:
+    print("Welcome " +name+ "!")
+else:
+    print("Your age does not meet the requirements. Game over.")
