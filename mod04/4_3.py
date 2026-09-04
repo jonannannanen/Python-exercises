ed904387993bf8ae75d92e210125f178b6a8b2c8
petyrfishfinger <jossu.haa@gmail.com>
2026-09-04 16:20:20 +0300

    hemoglobin

--- mod04/4_3.py
+++ mod04/4_3.py
@@ -3,13 +3,24 @@
 # A normal hemoglobin value for adult females is between 117-155 g/l.
 # A normal hemoglobin value for adult males is between 134-167 g/l.
 
-gender = print(input("Pick your gender: Male or Female"))
+gender = input("State your gender: ")
 
-if gender == Male:
-    print(input("Insert your hemoglobin: "))
-    if hemoglobin [134 - 167]:
-        print(input("Your hemoglobin is normal."))
-if gender == Female:
-    print(input("Insert your hemoglobin: "))
-    if hemoglobin [117 - 155]:
-        print(input("Your hemoglobin is normal."))
+if gender == "Male":
+    hemoglobin = int(input("Insert your hemoglobin: "))
+    print(hemoglobin, "g/l")
+    if 134 <= hemoglobin <= 167:
+        print("Your hemoglobin is normal.")
+    elif hemoglobin > 167:
+        print("Your hemoglobin is too high.")
+    elif hemoglobin < 134:
+        print("Your hemoglobin is too low.")
+
+if gender == "Female":
+    hemoglobin = int(input("Insert your hemoglobin: "))
+    print(hemoglobin, "g/l")
+    if 117 <= hemoglobin <= 155:
+        print("Your hemoglobin is normal.")
+    elif hemoglobin > 155:
+        print("Your hemoglobin is too high.")
+    elif hemoglobin < 117:
+        print("Your hemoglobin is too low.")
